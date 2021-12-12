1e2bffa43da99352ddef1accc820dd7df003a9f4
Biel Camprubí <bielcamprubi@gmail.com>
2021-12-12 20:59:24 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:time_tracker/page_activities.dart';
+import 'package:time_tracker/PageIntervals.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TimeTracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+            subtitle1: TextStyle(fontSize: 20.0),
+            bodyText2: TextStyle(fontSize: 20.0)),
+      ),
+      home: PageActivities(),
+    );
+  }
+}
